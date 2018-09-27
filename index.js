4a6ad602ff07a6f74f7a57da9080152cbe0bd70b
Edouard de Laage <edouarddelaage@gmail.com>
2018-09-27 13:43:17 +0200

    Inject data on front

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const axios = require('axios')
 
+let posts = []
+
 axios.get('http://reddit.com/r/aww.json')
     .then(response => {
-        console.log(response.data.data.children)
+        let posts = response.data.data.children
+        this.renderPosts(posts)
     })
     .catch(err => {
         console.log(err)
     })
+
+function renderPosts(posts) {
+    posts.forEach(element => {
+        document.getElementById('posts').innerHTML = document.getElementById('posts').innerHTML +
+        `
+            <li class="list-group-item d-flex align-items-center">
+                <img src="${element.data.thumbnail}" alt="thumb" class="thumbnail" />
+                <div>${element.data.title}</div>
+            </li>
+        `
+    });
+}
